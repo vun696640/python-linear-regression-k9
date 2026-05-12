e126fe9a25aa2dee625d18422188cb8ac159da79
vun696640 <125890057+vun696640@users.noreply.github.com>
2026-05-12 18:59:45 +0700

    Update visualizer.py

--- src/visualizer.py
+++ src/visualizer.py
@@ -146,4 +146,3 @@ class Visualizer:
 
         print("\nĐã vẽ đồ thị hồi quy tuyến tính.")
         print("Đã lưu biểu đồ vào file: regression_chart.png")
-        display(Image("/content/python-linear-regression-k9/regression_chart.png"))
